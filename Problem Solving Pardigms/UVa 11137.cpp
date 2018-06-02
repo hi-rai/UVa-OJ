5a6211726479f0efe94aeb5ff6426e1a76a599a4
Himanshu Rai <36773027+himanshu520@users.noreply.github.com>
2018-06-02 22:07:10 +0530

    Create UVa 11137.cpp

--- Problem Solving Pardigms/UVa 11137.cpp
+++ Problem Solving Pardigms/UVa 11137.cpp
@@ -1,3 +1,4 @@
+//Using top-down DP
 #include<bits/stdc++.h>
 #define SZ 10005
 using namespace std;
@@ -25,3 +26,28 @@ int main() {
     while(cin >> V) cout << calc(V, 0) << endl;
     return 0;
 }
+
+//Using bottom-up DP
+/*
+#include<bits/stdc++.h>
+#define SZ 10005
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    long long memo[SZ], coins[100], n = 25, V = 10000;
+    for (int i = 1; i <= 25; i++)
+        coins[i - 1] = i * i * i;
+    memset(memo, 0, sizeof memo), memo[0] = 1;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < V; j++)
+            if (j + coins[i] <= V)
+                memo[j + coins[i]] += memo[j];
+    while (cin >> V) cout << memo[V] << endl;
+    return 0;
+}*/
